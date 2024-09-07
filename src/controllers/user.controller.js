882718625216d3107a7cc3171ad964853b550669
shubham-goyal-0811 <shubhamsubbi1@gmail.com>
2024-09-07 22:16:03 +0530

    pipelines added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -318,6 +318,76 @@ const updateuserCoverImage = asyncHandler( async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,coverImage,"Cover image updated successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async (req,res)=>{
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new ApiError(400,"No username found");
+    }
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username : username?.toLowerCase().trim()
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "channel",
+                as : "subscribers"
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "subscriber",
+                as : "subscribedTo" 
+            }
+        },
+        {
+            $addFields : {
+                subscribersCount : {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount : {
+                    $size : "$subscribedTo"
+                },
+                isSubscribed : {
+                    $condition :{
+                        if : {
+                            $in : [req.user?._id,"$subscribers.subscriber"]
+                        },
+                        then : true,
+                        else : false
+                    }
+                }
+            }
+        },
+        {
+            $project : {
+                fullName : 1,
+                username : 1,
+                subscribersCount : 1,
+                channelsSubscribedToCount : 1,
+                isSubscribed : 1,
+                avatar : 1,
+                coverImage : 1,
+                email : 1
+            }   
+        }
+
+
+    ]);
+
+    if(!channel?.length){
+        throw new ApiError(400,"channel does not exist");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,channel[0],"channel fetched successfully"));
+})
 
 
 export { registerUser,
@@ -328,4 +398,5 @@ export { registerUser,
     getCurrentUser,
     updateAccountDetails,
     updateuserAvatar,
-    updateuserCoverImage }
+    updateuserCoverImage,
+    getUserChannelProfile }
