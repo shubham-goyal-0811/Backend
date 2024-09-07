e26ffedcbeb6e62bfffc293a1ead2fa79b4c0b25
shubham-goyal-0811 <shubhamsubbi1@gmail.com>
2024-09-08 00:59:06 +0530

    routes added for channel and history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -389,6 +389,55 @@ const getUserChannelProfile = asyncHandler(async (req,res)=>{
     .json(new ApiResponse(200,channel[0],"channel fetched successfully"));
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = User.aggregate([
+        {
+            $match:{
+                _id : new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup : {
+                from : "videos",
+                localField : "watchHistory",
+                foreignField : "_id",
+                as : "watchHistory",
+                pipeline:[
+                    {
+                        $lookup : {
+                            from : "users",
+                            localField : "owner",
+                            foreignField : "_id",
+                            as : "owner",
+                            pipleline :[
+                                {
+                                    $project : {
+                                        fullName : 1,
+                                        username : 1,
+                                        avatar : 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first : "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    if(!user?.length){
+        throw new ApiError(400,"user not found");
+    }
+    return res.status(200)
+    .json(200,user[0].watchHistory,"watch history fetched successfully");
+})
+
 
 export { registerUser,
     loginUser,
@@ -399,4 +448,5 @@ export { registerUser,
     updateAccountDetails,
     updateuserAvatar,
     updateuserCoverImage,
-    getUserChannelProfile }
+    getUserChannelProfile,
+    getWatchHistory }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -22,5 +22,12 @@ router.route("/login").post(loginUser);
 //secured route
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/updateAccountDetails").patch(verifyJWT,updateAccountDetails);
+router.route("/updateAvatar").patch(verifyJWT,upload.single("avatar"),updateAccountDetails);
+router.route("/updateCoverImage").patch(verifyJWT,upload.single("coverImage"),updateAccountDetails);
+router.route("/c/:username ").get(verifyJWT,getUserChannelProfile);
+router.route("/history").get(verifyJWT,getWatchHistory);
 
 export default router
